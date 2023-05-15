2ffd633a08445679b210af8ed2e5b1a3b0a6113d
John Ray <jray22@msu.idserve.net>
2023-05-15 15:40:38 -0400

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by John Ray on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
